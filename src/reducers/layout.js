1d2675e0b895a0bcb950074f6d201157114c13cf
MinJun2 <kaf829@naver.com>
2025-09-16 16:15:36 +0900

    레이아웃 색상 일치화를 위한 코드 수정

--- src/reducers/layout.js
+++ src/reducers/layout.js
@@ -14,7 +14,7 @@ import config from "../config";
 export const DashboardThemes = {
   LIGHT: "light",
   DARK: "dark",
-  WARNING: "#F9A70D", // #FEBE69
+  WARNING: "#FEB049", // #FEBE69
   DANGER: "#F5695A",
   SUCCESS: "#3BBF97",
   INFO: "#12B4DE",
